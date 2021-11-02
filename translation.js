71efa7bfe184e315345a821db5e0326024eb6143
Mr-Quin <hr82003@gmail.com>
2021-11-02 01:03:23 -0700

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,33 +1,5 @@
 // Imports the Google Cloud client library
 const {Translate} = require('@google-cloud/translate').v2;
-const got = require('got');
-const { JSDOM } = require("jsdom");
-
-const imageRegex = /(?<=\.)(png|jpe?g|gif)/
-const parensRegex = /^((?!\().)*$/;
-
-const isImage = (link) => {
-  // Return false if there is no href attribute.
-  if(typeof link.href === 'undefined')  return false 
-
-  return imageRegex.test(link.href.includes)
-};
-
-const noParens = (link) => {
-  return parensRegex.test(link.textContent);
-};
-
-const scrape = async (url) => {
-  const response = await got(url);
-  const dom = new JSDOM(response.body);
-
-  // Create an Array out of the HTML Elements for filtering using spread syntax.
-  const nodeList = [...dom.window.document.querySelectorAll('a')];
-
-  return nodeList.filter(isImage).filter(noParens).map(link => 
-    link.href
-  )
-}
 
 // Creates a client
 const translate = new Translate();
@@ -45,17 +17,13 @@ exports.entry = async (req, res) => {
     return res.status(400).json({msg: 'Bad request', reason: 'Method not allowed'})
   }
 
-  const {text, lang, target, url} = req.body
+  const {text, lang, target} = req.body
 
-  const result = await scrape(url)
+  if (!text || !target) {
+    return res.status(400).json({msg: 'Bad request', reason: 'Missing required parameters'})
+  }
 
-  return res.json(result)
-
-  // if (!text || !target) {
-  //   return res.status(400).json({msg: 'Bad request', reason: 'Missing required parameters'})
-  // }
-
-  // const translation = await translateText(text, target)
+  const translation = await translateText(text, target)
 
   res.json(translation)
 };
